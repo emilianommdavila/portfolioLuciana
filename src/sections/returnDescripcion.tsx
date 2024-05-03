4fddb66aa9e4c1afb0d2fb4e28cb39cccaea6248
emilianommdavila <emilianommdavila@gmail.com>
2024-05-03 15:04:33 -0300

    cambiofoto

--- src/sections/returnDescripcion.tsx
+++ src/sections/returnDescripcion.tsx
@@ -6,7 +6,7 @@ function ReturnDescripcion() {
   return (
     <section id="descripcion">
     <div className='descripcion'>
-      <img src="https://i.imgur.com/HBUf9Uz.jpg" alt="MDN" id='fotoPerfil'/>
+      <img src="https://i.imgur.com/Fjn1JTu.jpeg" alt="MDN" id='fotoPerfil'/>
       <h1 id='saludoPrincipal'>¡Hola! Soy Luciana</h1>    
       <h3 id='ubicacion'>Mendoza, Argentina</h3>
       <div id='descripcion'>Estudiante de la Tecnicatura Universitaria en Diseño y Animación Digital. Tengo 24 años y me encuentro en busca de mi primera experiencia en el área del modelado y animación 3D.</div>
