f2bf883b02d69e7f2068258bfa9eb3c64d943b07
Emiliano Davila <emilianommdavila@gmail.com>
2024-05-23 00:11:40 -0300

    fafs

--- src/sections/returnContacto.tsx
+++ src/sections/returnContacto.tsx
@@ -14,7 +14,7 @@ function ReturnContacto() {
         <h3 className='dato'>https://www.behance.net/lucianammateo</h3></a>
 
       
-      <a className='bloqueContacto' href="https://drive.google.com/file/d/1Rp3P09_LWwtsr-2SNy-mD_ZViKEO0QPL/view?usp=sharing">
+      <a className='bloqueContacto' href="https://drive.google.com/file/d/113phDR2YsQEM9kV2vdJYdkg9x9Gpqm-t/view">
         <img className="imgContacto" src="https://i.imgur.com/lT0Caej.png" alt="" />
         <h3 className='dato'>Curriculum Vitae </h3></a>
 
