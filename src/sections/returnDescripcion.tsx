00861eb7d875cd88b6dbbb41f32b107a2beb672b
Emiliano Davila <emilianommdavila@gmail.com>
2024-07-26 00:28:07 -0300

    fasdfs

--- src/sections/returnDescripcion.tsx
+++ src/sections/returnDescripcion.tsx
@@ -7,10 +7,8 @@ function ReturnDescripcion() {
     <section id="descripcion">
     <div className='descripcion'>
       <img src="https://i.imgur.com/Fjn1JTu.jpeg" alt="MDN" id='fotoPerfil'/>
-      <h1 id='saludoPrincipal'>¡Hola! Soy Luciana</h1>    
       <h3 id='ubicacion'>Mendoza, Argentina</h3>
-      <div id='descripcion'>Estoy recien graduada en la Tecnicatura Universitaria en Diseño y Animación Digital de la Universidad Siglo 21. Tengo 24 años y estoy entusiasmada por encontrar mi primera experiencia laboral en el campo. Me apasiona crear y dar vida a ideas a través del arte digital, y estoy ansiosa por aplicar mis habilidades y aprender en un entorno profesional.
-<br/>¡Te invito a explorar mis trabajos!
+      <div id='descripcion'>¡Hola! Mi nombre es Luciana Mateo y estoy recientemente graduada en la Tecnicatura Universitaria en Diseño y Animación Digital de la Universidad Siglo 21. Tengo 24 años y estoy entusiasmada por encontrar mi primera experiencia laboral en el campo. Me apasiona crear y dar vida a ideas a través del arte digital, y estoy ansiosa por aplicar mis habilidades y aprender en un entorno profesional.
 </div>
       <img src="https://i.imgur.com/XZNCh1Q.png" alt="MDN" id='fotoSiglo21'/>
     </div>
