0534827f9779ba86f0fa3a2353ea77f6f724ccf2
Emiliano Davila <emilianommdavila@gmail.com>
2024-07-26 00:25:46 -0300

    fasdfs

--- src/sections/returnDescripcion.tsx
+++ src/sections/returnDescripcion.tsx
@@ -9,7 +9,7 @@ function ReturnDescripcion() {
       <img src="https://i.imgur.com/Fjn1JTu.jpeg" alt="MDN" id='fotoPerfil'/>
       <h1 id='saludoPrincipal'>¡Hola! Soy Luciana</h1>    
       <h3 id='ubicacion'>Mendoza, Argentina</h3>
-      <div id='descripcion'>Estudiante de la Tecnicatura Universitaria en Diseño y Animación Digital. Tengo 24 años y estoy entusiasmada por encontrar mi primera experiencia laboral en el campo del diseño y la animación. Me apasiona crear y dar vida a ideas a través del arte digital, y estoy ansiosa por aplicar mis habilidades y aprender en un entorno profesional. 
+      <div id='descripcion'>Estoy recien graduada en la Tecnicatura Universitaria en Diseño y Animación Digital de la Universidad Siglo 21. Tengo 24 años y estoy entusiasmada por encontrar mi primera experiencia laboral en el campo. Me apasiona crear y dar vida a ideas a través del arte digital, y estoy ansiosa por aplicar mis habilidades y aprender en un entorno profesional.
 <br/>¡Te invito a explorar mis trabajos!
 </div>
       <img src="https://i.imgur.com/XZNCh1Q.png" alt="MDN" id='fotoSiglo21'/>
